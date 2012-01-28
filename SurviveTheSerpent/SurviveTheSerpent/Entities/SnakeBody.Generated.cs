2c49cb1dbc1c8ceeadfb99e46c7511f9645a067d
thearmada100 <thearmada100@c4bc9a8f-7ab1-f75e-58ac-f2b6e0460a5c>
2012-01-28 23:45:23 +0000

    Added all art and minor fixes

--- SurviveTheSerpent/SurviveTheSerpent/Entities/SnakeBody.Generated.cs
+++ SurviveTheSerpent/SurviveTheSerpent/Entities/SnakeBody.Generated.cs
@@ -158,7 +158,7 @@ namespace SurviveTheSerpent.Entities
 		public virtual void PostInitialize()
 		{
 			X = 0f;
-			Y = 2f;
+			Y = 8f;
 			EntireSceneCurrentChainName = "Elbow";
 		}
 		public virtual void AddToManagersBottomUp(Layer layerToAddTo)

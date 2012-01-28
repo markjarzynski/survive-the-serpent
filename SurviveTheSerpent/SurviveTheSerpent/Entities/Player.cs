b34e1b94a596af0079cbe2931534767689885432
thearmada100 <thearmada100@c4bc9a8f-7ab1-f75e-58ac-f2b6e0460a5c>
2012-01-28 14:35:34 +0000

    Added directional motion for the player

--- SurviveTheSerpent/SurviveTheSerpent/Entities/Player.cs
+++ SurviveTheSerpent/SurviveTheSerpent/Entities/Player.cs
@@ -23,20 +23,63 @@ using Texture2D = Microsoft.Xna.Framework.Graphics.Texture2D;
 
 namespace SurviveTheSerpent.Entities
 {
+
 	public partial class Player
 	{
+        // Direction Code:
+        // U,D,L,R = directions
+        // S = standing still
+        private Direction direction;
+        private const double PLAYER_SPEED = .2;
+        private double velocity;
+
+        public enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right,
+            Still
+        };
+
 		private void CustomInitialize()
 		{
             this.X = 0;
             this.Y = 0;
+            this.direction = Direction.Still;
+            this.velocity = PLAYER_SPEED;
+
 
 		}
 
 		private void CustomActivity()
 		{
-
+            switch (direction)
+            {
+                case Direction.Up:
+                    X += (float)velocity;
+                    break;
+                case Direction.Down:
+                    X -= (float)velocity;
+                    break;
+                case Direction.Left:
+                    Y += (float)velocity;
+                    break;
+                case Direction.Right:
+                    Y -= (float)velocity;
+                    break;
+                case Direction.Still:
+                    break;
+                default:
+                    break;
+}
 
 		}
+
+        public void SetDirection( Direction newDirection )
+        {
+            direction = newDirection;
+        }
 
 		private void CustomDestroy()
 		{

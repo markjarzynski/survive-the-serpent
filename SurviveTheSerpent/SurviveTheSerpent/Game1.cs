e480667dd59ef41d27e284702a54a5e3f0cba0cf
kraytex@gmail.com <kraytex@gmail.com@c4bc9a8f-7ab1-f75e-58ac-f2b6e0460a5c>
2012-01-28 01:14:15 +0000

    Initial commit.

---
+++ SurviveTheSerpent/SurviveTheSerpent/Game1.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Media;
+
+using FlatRedBall;
+using FlatRedBall.Graphics;
+using FlatRedBall.Utilities;
+
+using SurviveTheSerpent.Screens;
+
+namespace SurviveTheSerpent
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+			BackStack<string> bs = new BackStack<string>();
+			bs.Current = string.Empty;
+
+			
+            // Frame rate is 30 fps by default for Windows Phone.
+            TargetElapsedTime = TimeSpan.FromTicks(333333);
+            graphics.IsFullScreen = true;
+            graphics.PreferredBackBufferWidth = 480;
+            graphics.PreferredBackBufferHeight = 800;
+
+        }
+
+        protected override void Initialize()
+        {
+            FlatRedBall.Graphics.Renderer.UseRenderTargets = false;
+            FlatRedBallServices.InitializeFlatRedBall(this, graphics);
+
+            //Screens.ScreenManager.Start(typeof(FlatRedBallXNATemplate.Screens.TestScreen).FullName);
+
+            base.Initialize();
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            FlatRedBallServices.Update(gameTime);
+            
+			Screens.ScreenManager.Activity();
+            
+			base.Update(gameTime);
+        }
+
+
+        protected override void Draw(GameTime gameTime)
+        {
+            FlatRedBallServices.Draw();
+
+            base.Draw(gameTime);
+        }
+    }
+}

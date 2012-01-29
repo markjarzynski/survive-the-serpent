cf7ace9696e408eeb40687b4e417e68acb1ce987
MaverickHunterDN <MaverickHunterDN@c4bc9a8f-7ab1-f75e-58ac-f2b6e0460a5c>
2012-01-29 13:28:00 +0000

    - added cs files for win/lose screens

---
+++ trunk/SurviveTheSerpent/SurviveTheSerpent/Screens/LoseScreen.Generated.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FlatRedBall.Math.Geometry;
+using FlatRedBall.AI.Pathfinding;
+using FlatRedBall.Input;
+using FlatRedBall.IO;
+using FlatRedBall.Instructions;
+using FlatRedBall.Math.Splines;
+using FlatRedBall.Utilities;
+using BitmapFont = FlatRedBall.Graphics.BitmapFont;
+
+using Cursor = FlatRedBall.Gui.Cursor;
+using GuiManager = FlatRedBall.Gui.GuiManager;
+
+#if XNA4
+using Color = Microsoft.Xna.Framework.Color;
+#else
+using Color = Microsoft.Xna.Framework.Graphics.Color;
+#endif
+
+#if FRB_XNA || SILVERLIGHT
+using Keys = Microsoft.Xna.Framework.Input.Keys;
+using Vector3 = Microsoft.Xna.Framework.Vector3;
+using Texture2D = Microsoft.Xna.Framework.Graphics.Texture2D;
+using Microsoft.Xna.Framework.Media;
+#endif
+
+// Generated Usings
+using FlatRedBall.Broadcasting;
+using SurviveTheSerpent.Entities;
+using FlatRedBall;
+
+namespace SurviveTheSerpent.Screens
+{
+	public partial class LoseScreen : Screen
+	{
+		// Generated Fields
+		#if DEBUG
+		static bool HasBeenLoadedWithGlobalContentManager = false;
+		#endif
+		private Scene SceneFile;
+
+
+		public LoseScreen()
+			: base("LoseScreen")
+		{
+		}
+
+        public override void Initialize(bool addToManagers)
+        {
+			// Generated Initialize
+			LoadStaticContent(ContentManagerName);
+			SceneFile = FlatRedBallServices.Load<Scene>("content/screens/losescreen/scenefile.scnx", ContentManagerName);
+
+
+
+			PostInitialize();
+			if(addToManagers)
+			{
+				AddToManagers();
+			}
+
+        }
+        
+// Generated AddToManagers
+
+        public override void AddToManagers()
+        {
+			AddToManagersBottomUp();
+			CustomInitialize();
+
+        }
+
+
+		public override void Activity(bool firstTimeCalled)
+		{
+			// Generated Activity
+			if(!IsPaused)
+			{
+
+			}
+			else
+			{
+			}
+			base.Activity(firstTimeCalled);
+			if (!IsActivityFinished)
+			{
+				CustomActivity(firstTimeCalled);
+			}
+
+
+				// After Custom Activity
+			SceneFile.ManageAll();
+		
+		
+            
+		}
+
+		public override void Destroy()
+		{
+			// Generated Destroy
+			SceneFile.RemoveFromManagers(ContentManagerName != "Global");
+
+
+
+			base.Destroy();
+
+			CustomDestroy();
+
+		}
+
+		// Generated Methods
+		public virtual void PostInitialize()
+		{
+		}
+		public virtual void AddToManagersBottomUp()
+		{
+			SceneFile.AddToManagers(mLayer);
+
+		}
+		public virtual void ConvertToManuallyUpdated()
+		{
+			SceneFile.ConvertToManuallyUpdated();
+		}
+		public static void LoadStaticContent(string contentManagerName)
+		{
+			#if DEBUG
+			if(contentManagerName == FlatRedBallServices.GlobalContentManager)
+			{
+				HasBeenLoadedWithGlobalContentManager = true;
+			}
+			else if(HasBeenLoadedWithGlobalContentManager)
+			{
+				throw new Exception("This type has been loaded with a Global content manager, then loaded with a non-global.  This can lead to a lot of bugs");
+			}
+			#endif
+			CustomLoadStaticContent(contentManagerName);
+		}
+		object GetMember(string memberName)
+		{
+			switch(memberName)
+			{
+				case "SceneFile":
+					return SceneFile;
+			}
+			return null;
+		}
+
+
+	}
+}
